71133b369d431ed61dbd63b66d66058e8eba7b7d
oribe(oribe1115) <50443000+oribe1115@users.noreply.github.com>
2022-08-14 20:54:32 +0900

    logを消した

--- webapp/golang/main.go
+++ webapp/golang/main.go
@@ -89,10 +89,10 @@ func main() {
 	}()
 
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	e.Debug = false
+	e.Logger.SetLevel(log.ERROR)
 
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(cacheControllPrivate)
 
